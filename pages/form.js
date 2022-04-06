ce333e27039fc86890b4075b6b3b1023e058bfd7
Natalio Di Giacomo <natadg@outlook.com>
2022-04-06 19:20:10 -0300

    formspark

--- pages/form.js
+++ pages/form.js
@@ -6,31 +6,26 @@ import { useState } from 'react'
 
 const Form = () => {
 
-	const [name, setName] = useState('')
-	const [email, setEmail] = useState('')
-	const [message, setMessage] = useState('')
-	const [submited, setSubmitted] = useState(false)
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [message, setMessage] = useState('');
 
-	const handleSubmit =  async (e) => { 
-		e.preventDefault()
-		console.log('Sending')
-			let data = {
-		    name,
-		    email,
-		    message
-		 }
-		fetch('/api/contact', {
-	    	method: 'POST',
-	    	headers: {
-	      	'Accept': 'application/json, text/plain, */*',
-	      	'Content-Type': 'application/json'
-	    },
-    		body: JSON.stringify(data),
-
-  		});
-		await push('formok');
-
-}
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		await fetch(FORMSPARK_ACTION_URL, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		},
+		body: JSON.stringify({
+			name,
+			email,
+			message,
+		}),
+		});
+		alert("Form submitted");
+	};
 
 
 
@@ -38,23 +33,23 @@ const Form = () => {
 		<Layout footer={true}>
 			<div className="row p-4">
 				<div className="col-md-4 offset-md-4 ">	
-					<div className="mb-3">
-					  <label for="exampleFormControlInput1" className="form-label text-success ">Name:</label>
-					  <input type='text' onChange={(e)=>{setName(e.target.value)}} name='name'  className="form-control rounded" placeholder=""/>
-					</div>
-					<div className="mb-3">
-					  <label for="exampleFormControlInput1" className="form-label text-success">Email:</label>
-					  <input type='email' onChange={(e)=>{setEmail(e.target.value)}} name='email' className="form-control rounded" placeholder="name@example.com"/>
-					</div>
-					<div className="mb-3">
-					  <label for="exampleFormControlTextarea1" className="form-label text-success">Message:</label>
-					  <textarea type='text' onChange={(e)=>{setMessage(e.target.value)}} name='message'  className="form-control rounded" rows="4"></textarea>
-					</div>
-					<div className="mb-3 text-center">
-						<Link href="formok">
-							<a onClick={(e)=>{handleSubmit(e)}} className="btn btn-outline-dark border-success rounded-pill more-style nav-link text-success my-4 bg-dark" >Send</a>
-						</Link>	
-					</div>
+					<form onSubmit={onSubmit}>
+						<div className="mb-3">
+							<label for="exampleFormControlInput1" className="form-label text-success ">Name:</label>
+							<input type='text' value={name} onChange={(e)=>setName(e.target.value)}   className="form-control rounded" placeholder=""/>
+						</div>
+						<div className="mb-3">
+							<label for="exampleFormControlInput1" className="form-label text-success">Email:</label>
+							<input type='email' value={email} onChange={(e)=>setEmail(e.target.value)} name='email' className="form-control rounded" placeholder="name@example.com"/>
+						</div>
+						<div className="mb-3">
+							<label for="exampleFormControlTextarea1" className="form-label text-success">Message:</label>
+							<textarea type='text'  value={message} onChange={(e) => setMessage(e.target.value)} name='message'  className="form-control rounded" rows="4"></textarea>
+						</div>
+						<div className="mb-3 text-center">
+							<button type="submit" className="btn btn-outline-dark border-success rounded-pill more-style nav-link text-success my-4 bg-dark">Send</button>
+						</div>
+					</form>
 				</div>
 			</div>
 		</Layout>	
