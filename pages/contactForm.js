4d3d619828499bf1b2c4f1aa799aff339f470db9
Natalio Di Giacomo <natadg@outlook.com>
2022-04-07 09:10:08 -0300

    form update

--- pages/contactForm.js
+++ pages/contactForm.js
@@ -6,50 +6,26 @@ import { useState } from 'react'
 
 const ContactForm = () => {
 
-	const [name, setName] = useState('');
-	const [email, setEmail] = useState('');
-	const [message, setMessage] = useState('');
-
-	const onSubmit = async (e) => {
-		e.preventDefault();
-		await fetch(FORMSPARK_ACTION_URL, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-			Accept: "application/json",
-		},
-		body: JSON.stringify({
-			name,
-			email,
-			message,
-		}),
-		});
-		alert("Form submitted");
-	};
-
 
 
 	return(
 		<Layout footer={true}>
 			<div className="row p-4">
-				<div className="col-md-4 offset-md-4 ">	
-					<form onSubmit={onSubmit}>
-						<div className="mb-3">
-							<label for="exampleFormControlInput1" className="form-label text-success ">Name:</label>
-							<input type='text' value={name} onChange={(e)=>setName(e.target.value)}   className="form-control rounded" placeholder=""/>
-						</div>
-						<div className="mb-3">
-							<label for="exampleFormControlInput1" className="form-label text-success">Email:</label>
-							<input type='email' value={email} onChange={(e)=>setEmail(e.target.value)} name='email' className="form-control rounded" placeholder="name@example.com"/>
-						</div>
-						<div className="mb-3">
-							<label for="exampleFormControlTextarea1" className="form-label text-success">Message:</label>
-							<textarea type='text'  value={message} onChange={(e) => setMessage(e.target.value)} name='message'  className="form-control rounded" rows="4"></textarea>
-						</div>
-						<div className="mb-3 text-center">
-							<button type="submit" className="btn btn-outline-dark border-success rounded-pill more-style nav-link text-success my-4 bg-dark">Send</button>
-						</div>
-					</form>
+				<div className="col-md-4 offset-md-4 ">
+				<form action="https://submit-form.com/21WJU6nd">
+					<label for="name" className="form-label text-success">Name</label>	
+					<input type="text" id="name" name="name" placeholder="Name" required="" />
+					<label for="email" className="form-label text-success">Email</label>
+					<input type="email" id="email" name="email" placeholder="Email" required="" />
+					<label for="message" className="form-label text-success">Message</label>
+					<textarea
+						id="message"
+						name="message"
+						placeholder="Message"
+						required=""
+					></textarea>
+					<button type="submit" className="btn btn-outline-dark border-success rounded-pill more-style nav-link text-success my-4 bg-dark">Send</button>
+				</form>
 				</div>
 			</div>
 		</Layout>	
