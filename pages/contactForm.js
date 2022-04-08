7dcb9ec7bde734d3da0156b25c042097834d1394
Natalio Di Giacomo <natadg@outlook.com>
2022-04-07 23:24:07 -0300

    form update

--- pages/contactForm.js
+++ pages/contactForm.js
@@ -10,24 +10,25 @@ const ContactForm = () => {
 
 	return(
 		<Layout footer={true}>
-			<div className="row p-4">
-				<div className="col-md-4 offset-md-4 ">
-				<form action="https://submit-form.com/21WJU6nd">
-					<label for="name" className="form-label text-success">Name</label>	
-					<input type="text" id="name" name="name" placeholder="Name" required="" />
-					<label for="email" className="form-label text-success">Email</label>
-					<input type="email" id="email" name="email" placeholder="Email" required="" />
-					<label for="message" className="form-label text-success">Message</label>
-					<textarea
-						id="message"
-						name="message"
-						placeholder="Message"
-						required=""
-					></textarea>
-					<button type="submit" className="btn btn-outline-dark border-success rounded-pill more-style nav-link text-success my-4 bg-dark">Send</button>
-				</form>
-				</div>
-			</div>
+			<form action="https://submit-form.com/21WJU6nd" className="formulario">
+			<input
+    			type="hidden"
+    			name="_redirect"
+    			value="https://natalio-di-giacomo-portafolio.vercel.app/"
+			/>
+				<label for="name">Name</label>
+				<input type="text" id="name" name="name" placeholder="Name" required/>
+				<label for="email">Email</label>
+				<input type="email" id="email" name="email" placeholder="Email"  pattern="^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$"/>
+				<label for="message">Message</label>
+				<textarea
+    				id="message"
+    				name="message"
+    				placeholder="Message"
+    				required
+				></textarea>
+				<button type="submit">Send</button>
+			</form>
 		</Layout>	
 		)
 	
